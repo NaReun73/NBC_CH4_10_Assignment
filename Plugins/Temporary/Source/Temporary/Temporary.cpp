f8512981019b239fff678b00b91c769403ed6b4e
NaReun73 <rnlcksg37@gmail.com>
2026-06-16 17:27:14 +0900

    독립 플러그인 연결 완료

---
+++ Plugins/Temporary/Source/Temporary/Temporary.cpp
@@ -0,0 +1,15 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Temporary.h"
+
+void FTemporaryModule::StartupModule()
+{
+	UE_LOG(LogTemp, Log, TEXT("Temporary 플러그인 활성화"));
+}
+
+void FTemporaryModule::ShutdownModule()
+{
+	UE_LOG(LogTemp, Log, TEXT("Temporary 플러그인 종료"));
+}
+
+IMPLEMENT_MODULE(FTemporaryModule, Temporary);
